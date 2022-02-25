8f8f8d1149b3450475ecf314d1e7d92265368995
Kirefel <kirefelan@gmail.com>
2022-02-25 21:46:28 +1100

    Sein Lib

---
+++ SeinLib/CustomSeinAbilityManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace SeinLib
+{
+    public static class CustomSeinAbilityManager
+    {
+        private static List<CustomSeinAbility> customAbilities = new List<CustomSeinAbility>();
+
+        public static void Add(CustomSeinAbility ability)
+        {
+            customAbilities.Add(ability);
+        }
+
+        internal static void UpdateStateActive(SeinLogicCycle logicCycle)
+        {
+            foreach (var a in customAbilities)
+                a.SetStateActive(a.AllowAbility(logicCycle));
+        }
+
+        internal static void UpdateCharacterState()
+        {
+            foreach (var a in customAbilities)
+                CharacterState.UpdateCharacterState(a);
+        }
+    }
+}
