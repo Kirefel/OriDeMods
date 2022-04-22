bd77f2c25a61bf37cf0dad6b3b2b74ac5caa51fa
Kirefel <kirefelan@gmail.com>
2022-04-23 09:56:14 +1000

    Fix accessibility mod

--- AccessibilityMod/AccessibilityMod.cs
+++ AccessibilityMod/AccessibilityMod.cs
@@ -1,12 +1,12 @@
 ﻿using BaseModLib;
 using HarmonyLib;
 using OriDeModLoader;
-using UILib;
+using OriDeModLoader.UIExtensions;
 using UnityEngine;
 
 namespace AccessibilityMod
 {
-    public class ModInit : IMod
+    public class AccessibilityMod : IMod
     {
         public static BoolSetting cursorLock = new BoolSetting("Cursor Lock", true);
         public static FloatSetting newfloat = new FloatSetting("It's a float!", 0);
@@ -24,7 +24,9 @@ namespace AccessibilityMod
             Cursor.lockState = cursorLock ? CursorLockMode.Confined : CursorLockMode.None;
             // Settings.CursorLock.OnChanged += value => Cursor.lockState = value ? CursorLockMode.Confined : CursorLockMode.None;
 
-            CustomMenuManager.RegisterOptionsScreen<OptionsScreen>("Test", 100);
+            CustomMenuManager.RegisterOptionsScreen<OptionsScreen>("Accessibility", 100);
+            //CustomSeinAbilityManager.Add<MyAbility>("3962d401-67ee-48c0-86fa-7ab430ea2ddd");
+
         }
 
         public void Unload()
@@ -38,8 +40,24 @@ namespace AccessibilityMod
     {
         public override void InitScreen()
         {
-            AddToggle(ModInit.cursorLock, "Whether the cursor should be locked to the screen");
-            AddSlider(ModInit.newfloat, -1f, 1f, 0.2f, "[-1, 1] step 0.2");
+            AddToggle(AccessibilityMod.cursorLock, "Whether the cursor should be locked to the screen");
+            AddSlider(AccessibilityMod.newfloat, -1f, 1f, 0.2f, "[-1, 1] step 0.2");
         }
     }
+
+    //public class MyAbility : CustomSeinAbility
+    //{
+    //    public override bool AllowAbility(SeinLogicCycle logicCycle)
+    //    {
+    //        return !logicCycle.Sein.Abilities.Swimming.IsSwimming;
+    //    }
+
+    //    public override void UpdateCharacterState()
+    //    {
+    //        if (Input.GetKey(KeyCode.K))
+    //        {
+    //            Debug.Log("You are holding K");
+    //        }
+    //    }
+    //}
 }
---
+++ AccessibilityMod/MaxAreaMapZoomPatch.cs
@@ -0,0 +1,29 @@
+﻿using HarmonyLib;
+
+namespace AccessibilityMod
+{
+    [HarmonyPatch(typeof(AreaMapNavigation), nameof(AreaMapNavigation.Awake))]
+    class MaxAreaMapZoomPatch
+    {
+        static void Postfix(AreaMapNavigation __instance)
+        {
+            __instance.AreaMapZoomLevel = 1;
+        }
+    }
+
+    //public class MyAbility : CustomSeinAbility
+    //{
+    //    public override bool AllowAbility(SeinLogicCycle logicCycle)
+    //    {
+    //        return !logicCycle.Sein.Abilities.Swimming.IsSwimming;
+    //    }
+
+    //    public override void UpdateCharacterState()
+    //    {
+    //        if (Input.GetKey(KeyCode.K))
+    //        {
+    //            Debug.Log("You are holding K");
+    //        }
+    //    }
+    //}
+}
