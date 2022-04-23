24c80484d37dac3d79e1096330fa50cbf83ebf9c
Kirefel <kirefelan@gmail.com>
2022-04-23 10:00:35 +1000

    Update projects

--- UISpeedMod/UISpeedMod.cs
+++ UISpeedMod/UISpeedMod.cs
@@ -3,21 +3,21 @@ using OriDeModLoader;
 
 namespace UISpeedMod
 {
-    public class ModInit : IMod
+    public class UISpeedMod : IMod
     {
-        public string Name => "UI Lib";
+        public string Name => "UI Speed Mod";
 
         Harmony harmony;
 
         public void Init()
         {
-            harmony = new Harmony("com.ori.uilib");
+            harmony = new Harmony("com.ori.uispeed");
             harmony.PatchAll();
         }
 
         public void Unload()
         {
-            harmony.UnpatchAll("com.ori.uilib");
+            harmony.UnpatchAll("com.ori.uispeed");
         }
     }
 }
